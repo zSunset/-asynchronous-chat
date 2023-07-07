89e2dd4e2df0ed1ffc9160c5b9b650a569174cb1
zSunset <85899844+zSunset@users.noreply.github.com>
2023-07-07 15:54:20 +0300

    lesson_6 (#6)

--- lesson_3/log/server_log_config.py
+++ lesson_3/log/server_log_config.py
@@ -1,7 +1,7 @@
 import logging
 import os
 
-__directory = '/home/sunset/Рабочий стол/-asynchronous-chat/lesson_3/log_file/'
+__directory = '/home/sunset/Рабочий стол/временная папка/lesson_3/log_file/'
 __formater = logging.Formatter('%(asctime)-10s %(levelname)-6s %(message)-5s')
 __handler_client = logging.FileHandler(os.path.join(__directory, 'server.log'), mode='w')
 
