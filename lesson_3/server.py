10b2244e8efaee32d067cb237949aa75ddf1ca47
zSunset <85899844+zSunset@users.noreply.github.com>
2023-07-28 12:43:21 +0300

    lesson_11 (#11)

--- lesson_3/server.py
+++ lesson_3/server.py
@@ -5,18 +5,18 @@ import json
 import logging
 import select
 import time
+import threading
 from errors import IncorrectDataRecivedError
 from common.variables import *
 from common.utils import *
 from log.decorat_log import log
 from descrptrs import Port
 from metaclasses import ServerMaker
+from server_database import ServerStorage
 
-# Инициализация логирования сервера.
 logger = logging.getLogger('server')
 
 
-# Парсер аргументов коммандной строки.
 @log
 def arg_parser():
     parser = argparse.ArgumentParser()
@@ -27,43 +27,35 @@ def arg_parser():
     listen_port = namespace.p
     return listen_address, listen_port
 
-# Основной класс сервера
-class Server(metaclass=ServerMaker):
+
+class Server(threading.Thread, metaclass=ServerMaker):
     port = Port()
 
-    def __init__(self, listen_address, listen_port):
-        # Параментры подключения
+    def __init__(self, listen_address, listen_port, database):
         self.addr = listen_address
         self.port = listen_port
 
-        # Список подключённых клиентов.
+        self.database = database
         self.clients = []
 
-        # Список сообщений на отправку.
         self.messages = []
 
-        # Словарь содержащий сопоставленные имена и соответствующие им сокеты.
         self.names = dict()
+
+        super().__init__()
 
     def init_socket(self):
         logger.info(
             f'Запущен сервер, порт для подключений: {self.port} , адрес с которого принимаются подключения: {self.addr}. Если адрес не указан, принимаются соединения с любых адресов.')
-        # Готовим сокет
         transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         transport.bind((self.addr, self.port))
         transport.settimeout(0.5)
-
-        # Начинаем слушать сокет.
         self.sock = transport
         self.sock.listen()
 
-    def main_loop(self):
-        # Инициализация Сокета
+    def run(self):
         self.init_socket()
-
-        # Основной цикл программы сервера
         while True:
-            # Ждём подключения, если таймаут вышел, ловим исключение.
             try:
                 client, client_address = self.sock.accept()
             except OSError:
@@ -75,14 +67,12 @@ class Server(metaclass=ServerMaker):
             recv_data_lst = []
             send_data_lst = []
             err_lst = []
-            # Проверяем на наличие ждущих клиентов
             try:
                 if self.clients:
                     recv_data_lst, send_data_lst, err_lst = select.select(self.clients, self.clients, [], 0)
             except OSError:
                 pass
 
-            # принимаем сообщения и если ошибка, исключаем клиента.
             if recv_data_lst:
                 for client_with_message in recv_data_lst:
                     try:
@@ -91,7 +81,6 @@ class Server(metaclass=ServerMaker):
                         logger.info(f'Клиент {client_with_message.getpeername()} отключился от сервера.')
                         self.clients.remove(client_with_message)
 
-            # Если есть сообщения, обрабатываем каждое.
             for message in self.messages:
                 try:
                     self.process_message(message, send_data_lst)
@@ -101,8 +90,6 @@ class Server(metaclass=ServerMaker):
                     del self.names[message[DESTINATION]]
             self.messages.clear()
 
-    # Функция адресной отправки сообщения определённому клиенту. Принимает словарь сообщение, список зарегистрированых
-    # пользователей и слушающие сокеты. Ничего не возвращает.
     def process_message(self, message, listen_socks):
         if message[DESTINATION] in self.names and self.names[message[DESTINATION]] in listen_socks:
             send_message(self.names[message[DESTINATION]], message)
@@ -113,15 +100,13 @@ class Server(metaclass=ServerMaker):
             logger.error(
                 f'Пользователь {message[DESTINATION]} не зарегистрирован на сервере, отправка сообщения невозможна.')
 
-    # Обработчик сообщений от клиентов, принимает словарь - сообщение от клиента, проверяет корректность, отправляет
-    #     словарь-ответ в случае необходимости.
     def process_client_message(self, message, client):
         logger.debug(f'Разбор сообщения от клиента : {message}')
-        # Если это сообщение о присутствии, принимаем и отвечаем
         if ACTION in message and message[ACTION] == PRESENCE and TIME in message and USER in message:
-            # Если такой пользователь ещё не зарегистрирован, регистрируем, иначе отправляем ответ и завершаем соединение.
             if message[USER][ACCOUNT_NAME] not in self.names.keys():
                 self.names[message[USER][ACCOUNT_NAME]] = client
+                client_ip, client_port = client.getpeername()
+                self.database.user_login(message[USER][ACCOUNT_NAME], client_ip, client_port)
                 send_message(client, RESPONSE_200)
             else:
                 response = RESPONSE_400
@@ -130,18 +115,16 @@ class Server(metaclass=ServerMaker):
                 self.clients.remove(client)
                 client.close()
             return
-        # Если это сообщение, то добавляем его в очередь сообщений. Ответ не требуется.
         elif ACTION in message and message[ACTION] == MESSAGE and DESTINATION in message and TIME in message \
                 and SENDER in message and MESSAGE_TEXT in message:
             self.messages.append(message)
             return
-        # Если клиент выходит
         elif ACTION in message and message[ACTION] == EXIT and ACCOUNT_NAME in message:
-            self.clients.remove(self.names[ACCOUNT_NAME])
-            self.names[ACCOUNT_NAME].close()
-            del self.names[ACCOUNT_NAME]
+            self.database.user_logout(message[ACCOUNT_NAME])
+            self.clients.remove(self.names[message[ACCOUNT_NAME]])
+            self.names[message[ACCOUNT_NAME]].close()
+            del self.names[message[ACCOUNT_NAME]]
             return
-        # Иначе отдаём Bad request
         else:
             response = RESPONSE_400
             response[ERROR] = 'Запрос некорректен.'
@@ -149,13 +132,44 @@ class Server(metaclass=ServerMaker):
             return
 
 
+def print_help():
+    print('Поддерживаемые комманды:')
+    print('users - список известных пользователей')
+    print('connected - список подключенных пользователей')
+    print('loghist - история входов пользователя')
+    print('exit - завершение работы сервера.')
+    print('help - вывод справки по поддерживаемым командам')
+
+
 def main():
-    # Загрузка параметров командной строки, если нет параметров, то задаём значения по умоланию.
     listen_address, listen_port = arg_parser()
 
-    # Создание экземпляра класса - сервера.
-    server = Server(listen_address, listen_port)
-    server.main_loop()
+    database = ServerStorage()
+
+    server = Server(listen_address, listen_port, database)
+    server.daemon = True
+    server.start()
+
+    print_help()
+
+    while True:
+        command = input('Введите комманду: ')
+        if command == 'help':
+            print_help()
+        elif command == 'exit':
+            break
+        elif command == 'users':
+            for user in sorted(database.users_list()):
+                print(f'Пользователь {user[0]}, последний вход: {user[1]}')
+        elif command == 'connected':
+            for user in sorted(database.active_users_list()):
+                print(f'Пользователь {user[0]}, подключен: {user[1]}:{user[2]}, время установки соединения: {user[3]}')
+        elif command == 'loghist':
+            name = input('Введите имя пользователя для просмотра истории. Для вывода всей истории, просто нажмите Enter: ')
+            for user in sorted(database.login_history(name)):
+                print(f'Пользователь: {user[0]} время входа: {user[1]}. Вход с: {user[2]}:{user[3]}')
+        else:
+            print('Команда не распознана.')
 
 
 if __name__ == '__main__':
